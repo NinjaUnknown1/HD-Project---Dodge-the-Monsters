84b967de94f16328556f6224c87042f3cd7a255e
Caitlyn <caitlynsims32@gmail.com>
2018-06-08 11:04:19 +1000

    Added Edge Detection in Neural Network

--- NeuralNetwork_A1.py
+++ NeuralNetwork_A1.py
@@ -3,12 +3,20 @@ from PIL import ImageGrab
 import cv2
 import time
 
+def ProcessImage(image):
+	processedImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	processedImage = cv2.Canny(processedImage, threshold1=200, threshold2=300)
+	return processedImage
+
 while True:
         # Grab an image of the screen
-	printscreen_pil = ImageGrab.grab(bbox=(20,50,1020,840))
+	screen = np.array(ImageGrab.grab(bbox=(20,50,1020,840)))
+
+	newScreen = ProcessImage(screen)
 	
         # Show the image on the screen
-	cv2.imshow('window', np.array(printscreen_pil))
+	#cv2.imshow('window', np.array(screen))
+	cv2.imshow('window', newScreen)
 	cv2.moveWindow ('window', 1100, 20)
 	if cv2.waitKey(25) and 0xFF == ord('q'):
 		cv2.destroyAllWindows()
